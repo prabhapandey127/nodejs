f54585c1046bac7546e23e7757a8ec1babd9e24f
Prabhat <prabhat@Prabhats-MacBook-Air.local>
2022-12-02 02:45:36 +0530

    create static APIs

---
+++ data.js
@@ -0,0 +1,9 @@
+const data = [
+    {name:"Prabhat", age:22,email:"prabhatd@chetu.com"},
+    {name:"Prabha", age:22,email:"prabha@chetu.com"},
+    {name:"Sam", age:22,email:"sam@chetu.com"},
+    {name:"Peter", age:22,email:"peter@chetu.com"},
+
+]
+
+module.exports = data
--- index9.js
+++ index9.js
@@ -1,3 +1,15 @@
-const ch = require('chalk');
+// API
 
-console.log(chalk.blue('Hello world!'))
+//static data came from an other file
+const http = require('http');
+const data = require('./data');
+
+
+http.createServer((req,res)=>{
+
+
+    res.writeHead(200,{'Content-Type':'application/json'});
+    res.write(JSON.stringify(data));
+    res.end();
+
+}).listen(4500);
