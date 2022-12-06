e4adb8843e3b06cfe1c19b84823787c8c3a07338
Prabhat <prabhat@Prabhats-MacBook-Air.local>
2022-12-07 01:01:38 +0530

    dec 6th

--- index.js
+++ index.js
@@ -1,20 +1,73 @@
-//import {a} from './app';
-// const app = require('./app')
+const express = require('express');
 
-// console.log('hi')
 
-const fs = require('fs');
-// console.log(app)
-// console.log(app.z())
+const app = express();
 
-// const arr = [4,8,9,7,5,3]
+const path = require('path');
 
-// const res = arr.filter((item)=>{
-//     return item==4;
+const publicPath = path.join(__dirname, 'public')
+
+app.set('view engine', 'ejs')
+
+
+
+// text
+// app.get('', (req,res) => {
+//     res.send(`Hello , ${req.query.name} This is home page`)
+// })
+//html
+// app.get('/about', (req,res) => {
+//     res.send(`
+//         <input type="text" placeholder="Enter Name" value="${req.query.name}" />
+//         <button>Click</button>
+
+//     `)
 // })
 
-// console.log(res)
 
-fs.writeFileSync("Hello.txt","Hi this file")
+//json
+// app.get('/help', (req,res) => {
+//     res.send({
+//         name:'Prabhat',
+//         age:22
+//     })
+// })
 
-console.log(`Dir Name: ==>${__dirname}\nFile Name:==>${__filename}`);
+//page access with extension
+
+//app.use(express.static(publicPath));
+
+
+//page access without extension
+
+app.get('', (req, res) => {
+    res.sendFile(`${publicPath}/index.html`)
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(`${publicPath}/about.html`)
+})
+
+
+//ejs web page
+app.get('/profile', (req, res) => {
+
+    const user = {
+        name: "Prabhat", email: "prabhat@yopmail.com", age: 22, city: "Noida",
+        skills: ['PHP', 'JS', 'React', 'Laravel', 'C++']
+    };
+    res.render('profile', { user })
+})
+
+app.get('/login', (req, res) => {
+
+    res.render('login')
+})
+
+// 404 not found page
+app.get('*', (req, res) => {
+    res.sendFile(`${publicPath}/404.html`)
+})
+
+
+app.listen(4500)
